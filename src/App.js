749756ea679dba2eff19f388dc2005dd797d415d
cpbanna <chandrapalsinghrathore.it18@jecrc.ac.in>
2021-07-29 15:52:39 +0530

    Mysemse

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch, Link} from 'react-router-dom';
+import React from 'react';
+import Home from './components/Home'
+import FoodItem from './components/FoodItem'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Route exact path='/' component={Home} />
+      <Switch>
+            <Route exact path='/ourmenu' render={() => <div><h1>Our Menu</h1><Link to="/">HOME</Link></div>} />
+            <Route exact path='/services' render={() => <div><h1>Services</h1><Link to="/">HOME</Link></div>} />
+            <Route exact path='/about' render={() => <div><h1>About</h1><Link to="/">HOME</Link></div>} />
+            <Route exact path='/delivery' render={() => <div><h1>Delivery</h1><Link to="/">HOME</Link></div>} />
+            <Route exact path='/reservation' render={() => <div><h1>Reservation</h1><Link to="/">HOME</Link></div>} />
+            <Route expct path='/home/:id' component={FoodItem} />
+      </Switch>
+    </div>  
   );
 }
 
